5053494831aa08f93b3d4c4cdb9047af90dd8632
eduarda patu <maria.silva26@unemat.br>
2026-05-03 18:33:24 -0400

    feat: lógica para adicionar itens na lista

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,23 @@
 import { ref } from 'vue'
 const itemNovo = ref('')
 const lista = ref([])
+
+const adicionarItem = () => {
+  if (itemNovo.value.trim() !== '') {
+    lista.value.push(itemNovo.value)
+    itemNovo.value = ''
+  }
+}
 </script>
 <template>
-  <h1>Gerenciador de Compras</h1>
-</template>git 
+  <main>
+    <h1> Gerenciador de Compras</h1>
+    
+    <input v-model="itemNovo" @keyup.enter="adicionarItem" placeholder="Produto...">
+    <button @click="adicionarItem">Adicionar</button>
+    
+    <ul>
+      <li v-for="(item, index) in lista" :key="index">{{ item }}</li>
+    </ul>
+  </main>
+  </template>
