4d2d96c5d8baf5b82a7ad3d2d172265e00fdc33a
eduarda patu <maria.silva26@unemat.br>
2026-05-03 18:29:20 -0400

    Feat: variáveis reativas e estrutura inicial

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
-<script setup> 
+<script setup>
 import { ref } from 'vue'
-
-const itemNovo = ref('') // coloquei para guardar o que o usuário digitar
-const lista = ref([]) // coloquei para guardar a lista de itens
-
-const adicionarItem = () => {
-  if (itemNovo.value.trim() !== '') { // Coloquei para verificar se o campo não está vazio
-    lista.value.push(itemNovo.value) // Adiciona o novo item à lista
-    itemNovo.value = '' // Limpa o campo original
-  }
-}
-const removerProduto = (id) => {
-  lista.value.splice(id, 1) // Remove o item da lista com base no índice
-}
+const itemNovo = ref('')
+const lista = ref([])
 </script>
+<template>
+  <h1>Gerenciador de Compras</h1>
+</template>git 
